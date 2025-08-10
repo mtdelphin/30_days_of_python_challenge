487e7c54cc6ae53fb2e1b708946646ee2062be4f
M'po Frank <tcheytiba1999@gmail.com>
2025-08-10 18:42:08 +0100

    DAY-17 EXCEPTION HANDLING

---
+++ J17/exercice.py
@@ -0,0 +1,6 @@
+"""Day17 30 days of Python Programming"""
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_countries, es, ru = names
+
+print(nordic_countries, es, ru)
